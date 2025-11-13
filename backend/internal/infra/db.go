8724f4f2f7ec153930800855b8f353eae328dee8
RanyAS <ranyanastasia15@gmail.com>
2025-11-13 10:10:31 +0900

    Supabase auth

--- backend/internal/infra/db.go
+++ backend/internal/infra/db.go
@@ -20,12 +20,5 @@ func InitDB() {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 	defer conn.Close(context.Background())
-
-	// Example query to test connection
-	var version string
-	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
-		log.Fatalf("Query failed: %v", err)
-	}
-
-	log.Println("Connected to:", version)
+	log.Println("Database connection established")
 }
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	database "example.com/go-echo-crud/internal/infra"
 	"example.com/go-echo-crud/pkg/router"
 	"github.com/labstack/echo/v4"
@@ -11,13 +9,15 @@ import (
 func main() {
 	e := echo.New()
 
-	// Inisialisasi koneksi database
+	// DB接続
 	database.InitDB()
 
 	// Setup routes
 	router.Setup(e)
+	router.LoginRouter(e)
+	router.SignupRouter(e)
 
-	port := "8080"
-	fmt.Println("サーバー起動 http://localhost:" + port + "/")
-	e.Logger.Fatal(e.Start(":" + port))
+	// 	port := "8080"
+	// 	fmt.Println("サーバー起動 http://localhost:" + port + "/")
+	// 	e.Logger.Fatal(e.Start(":" + port))
 }
