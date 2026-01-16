2650a7091e9a4259f53d2c67064fc5cece7180f4
hsmt-T <ittuyo.hashi244@gmail.com>
2026-01-16 15:25:11 +0900

    dbがn度も初期化されてたのを修正　自身のtodoを表示できるように修正　実行確認済み

--- backend/internal/handler/todos_handler.go
+++ backend/internal/handler/todos_handler.go
@@ -11,12 +11,6 @@ import (
 type TodosHandler struct {
 	todosService service.ITodosService
 }
-
-// ミドルウェアできるまで
-type Req struct {
-	UserId string `json:"user_id"`
-}
-
 //--------------------------
 
 func NewTodosHandler(todosService service.ITodosService) *TodosHandler {
@@ -25,10 +19,12 @@ func NewTodosHandler(todosService service.ITodosService) *TodosHandler {
 
 func (h *TodosHandler) GetUserTodo(c echo.Context) error {
 
-	//ミドルウェアできたらuser_idもらってそのユーザーのTodoをもってくる
-	var UserId = "60c9c8df-2ba3-43de-954c-5aa1000aa28c"
+	userID, ok := c.Get("user_id").(string)
+	if !ok || userID == "" {
+		return echo.NewHTTPError(http.StatusUnauthorized, "unauthorized")
+	}
 
-	todos, err := h.todosService.GetUserTodo(UserId)
+	todos, err := h.todosService.GetUserTodo(userID)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{
 			"error": err.Error(),
